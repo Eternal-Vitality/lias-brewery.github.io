f9a31fb1f9af3dc2a6a48b11d3ac1afb50afa633
lias-brewery <145079902+lias-brewery@users.noreply.github.com>
2023-10-07 20:34:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const formEl = document.querySelector('.orderform');
+var form = document.getElementById("form")
 
-formEl.addEventListener('orderform', event => {
-  event.preventDefault();
+form.addEventListener('submit', function(e){
+  e.preventDefault();
   
-  const formData = new FormData(formEl);
-  const data = Object.fromEntries(formData);
-
-  console.log(data);
-});
+  // Addressing form inputs
+  var username = document.getElementById('username')
+  var order = document.getElementById('order')
+  var pickupLocation = document.getElementById('pickupLocation')
+  
+  fetch('https://discord.com/api/webhooks/1160371888074264606/fGHcjEEyuDLy1khriP89BaZGCpQhAIsSy4KkW0OKkk101znA_F0NUTpJxRkfs5aRga_u', {
+    method:'POST',
+    body:JSON.stringify({
+      title:username,
+      body:order,
+      id:pickupLocation
+    })
+    headers:{
+      "Content-Type":"application/json; charset=UTF-8"
+   }
+  })
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(data){
+    console.log(data);
+  })
+  
+})
